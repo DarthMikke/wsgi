a2417519bdeaae34c9bf3aae3acb5030cfbbe8b0
Michal Jan Warecki <mwarecki1@gmail.com>
2024-09-25 18:00:04 +0200

    Allow running other tags

--- examples/run.sh
+++ examples/run.sh
@@ -1,1 +1,7 @@
-docker run -v ./example.py:/app/asgi.py -p 8000:5000 ghcr.io/darthmikke/wsgi
+if [[ ! -z $1 ]]; then
+TAG=":$1"
+else
+TAG=""
+fi
+
+docker run --rm -it -v ./example.py:/app/asgi.py -p 8000:5000 ghcr.io/darthmikke/wsgi$TAG
